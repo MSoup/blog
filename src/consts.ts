07f8b40cf15fca3e569e2ecedcd0f45f7cae8df6
Dave <davenlu1989@gmail.com>
2023-08-06 15:39:02 +0900

    update site title, remove boilerplate markdown

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Cloud Soup";
+export const SITE_DESCRIPTION =
+  "A blog that discusses software engineering practices pertaining to cloud and AWS";
